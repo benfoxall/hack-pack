69576416c65c34a28a28d820eaeca9bdcb16b316
Ben Foxall <ben@pusher.com>
2016-06-20 16:54:09 +0100

    cors

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const pusher = new Pusher({
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use(express.static('public'))
 
 app.use(bodyParser.json())
